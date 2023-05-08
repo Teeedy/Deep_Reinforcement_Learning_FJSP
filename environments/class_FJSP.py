4ed0846510f76445e7f597aa3324d8a07f5dd8d0
linshan_ding@hust.edu.cn <linshan_ding@hust.edu.cn>
2023-05-08 10:49:07 +0800

    Define the based class FJSP of the scheduling environment

--- environments/class_FJSP.py
+++ environments/class_FJSP.py
@@ -121,17 +121,6 @@ class FJSP(Instance):
     """柔性作业车间调度类"""
     def __init__(self, DDT, M, S):
         Instance.__init__(self, DDT, M, S)
-        # 封装基本属性
-        self.step_count = 0  # 决策步
-        self.step_time = 0  # 时间点
-        self.last_observation_state = None  # 上一步观察到的状态 v(t-1)
-        self.observation_state = None  # 当前时间步的状态 v(t)
-        self.state_gap = None  # v(t) - v(t-1)
-        self.state = None  # s(t)
-        self.next_state = None  # 下一步状态  s(t+1)
-        self.reward = None  # 即时奖励
-        self.done = False  # 是否为终止状态
-
         # 实例化工件类型、工件、工序类型、工序和机器对象字典
         self.kind_task_dict = {(r, j): Tasks(r, j) for r in self.kind_tuple for j in self.task_r_dict[r]}  # 工序类型对象字典
         self.order_dict = {s: Order(s, self.time_arrive_s_dict[s], self.time_delivery_s_dict[s], self.count_sr_dict[s])
@@ -140,14 +129,14 @@ class FJSP(Instance):
         self.machine_dict = {m: Machine(m) for m in self.machine_tuple}  # 机器对象字典
         self.task_dict = {}  # (r,n,j) 工序对象字典 订单到达更新
         self.job_dict = {}  # (r,n)  # 工件对象字典
-        self.reset_parameter()  # 初始化参数对象中的列表和字典
-        # 机器加工工序类型的速率
-        self.process_rate_m_rj_dict = {m: {(r, j): 1 / self.time_mrj_dict[m][(r, j)]
-                                           for (r, j) in self.kind_task_m_dict[m]} for m in self.machine_tuple}  # 机器加工流体速率
+        self.process_rate_m_rj_dict = {m: {(r, j): 1 / self.time_mrj_dict[m][(r, j)] for (r, j) in self.kind_task_m_dict[m]} for m in self.machine_tuple}  # 机器加工流体速率
+
+        # 初始化参数对象中的列表和字典
+        self.reset_parameter()
         # 初始化各对象属性# 新订单到达后更新各字典对象
         self.reset_object_add(self.order_dict[0])
         # 初始化空闲机器列表和可选工序类型列表
-        self.machine_idle = self.idle_machine()  # 空闲机器编号列表
+        self.machine_idle_list = self.idle_machine()  # 空闲机器编号列表
         self.kind_task_available = self.kind_task_available()  # 可选工序类型编号列表
         print("成功定义FJSP类")
 
